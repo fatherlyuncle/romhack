648d7ba41274e9923e284a470a7bcdec2c12b021
fatherlyuncle <henningnick@gmail.com>
2020-06-25 13:48:09 -0400

    Fixed ItemUseOutOfBattle functions for fly/flash in item_use.h

--- include/item_use.h
+++ include/item_use.h
@@ -25,6 +25,8 @@ void ItemUseOutOfBattle_EvolutionStone(u8);
 void ItemUseOutOfBattle_Berry(u8);
 void ItemUseOutOfBattle_EnigmaBerry(u8);
 void ItemUseOutOfBattle_CannotUse(u8);
+void ItemUseOutOfBattle_Fly(u8);  //replace fly with item
+void ItemUseOutOfBattle_Flash(u8);//replace flash with item
 void ItemUseInBattle_PokeBall(u8);
 void ItemUseInBattle_StatIncrease(u8);
 void ItemUseInBattle_Medicine(u8);
