c64e558969aded788183750764b155dec4950cc8
fatherlyuncle <henningnick@gmail.com>
2020-06-25 22:42:45 -0400

    Edited strings.c and field_poison.c to survive poison outside battle at 1HP

--- src/field_poison.c
+++ src/field_poison.c
@@ -48,7 +48,7 @@ static void FaintFromFieldPoison(u8 partyIdx)
     struct Pokemon *pokemon = gPlayerParty + partyIdx;
     u32 status = STATUS1_NONE;
 
-    AdjustFriendship(pokemon, FRIENDSHIP_EVENT_FAINT_FIELD_PSN);
+    //AdjustFriendship(pokemon, FRIENDSHIP_EVENT_FAINT_FIELD_PSN);   //Survive poison so not needed
     SetMonData(pokemon, MON_DATA_STATUS, &status);
     GetMonData(pokemon, MON_DATA_NICKNAME, gStringVar1);
     StringGetEnd10(gStringVar1);
@@ -57,7 +57,7 @@ static void FaintFromFieldPoison(u8 partyIdx)
 static bool32 MonFaintedFromPoison(u8 partyIdx)
 {
     struct Pokemon *pokemon = gPlayerParty + partyIdx;
-    if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) == 0 && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+    if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) == 1 && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN) //Changed to 1 to survive poison
     {
         return TRUE;
     }
@@ -128,7 +128,7 @@ s32 DoPoisonFieldEffect(void)
         if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
         {
             hp = GetMonData(pokemon, MON_DATA_HP);
-            if (hp == 0 || --hp == 0)
+            if (hp == 1 || --hp == 1) //Survive poison outside battle
             {
                 numFainted++;
             }
