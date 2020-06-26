2205be85a61efbbdbaf4fdd739a84aaf16108669
fatherlyuncle <henningnick@gmail.com>
2020-06-25 22:56:58 -0400

    Added supersurf while hold B to src/field_player_avatar.c

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -637,8 +637,12 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
 
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
-    // speed 2 is fast, same speed as running
-        PlayerGoSpeed2(direction);
+    /*// speed 2 is fast, same speed as running
+        PlayerGoSpeed2(direction);*/
+		if (heldKeys & B_BUTTON) //supersurf while hold B 
+			PlayerGoSpeed4(direction);
+		else
+			PlayerGoSpeed2(direction);
         return;
     }
 
