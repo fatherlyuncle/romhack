87fb45922bbcddfb1c5873e842b9846bfcebab3b
fatherlyuncle <henningnick@gmail.com>
2020-06-28 15:13:25 -0400

    Hopefully fix regular moves being forgettable

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2321,11 +2321,11 @@ static void Task_HandleReplaceMoveInput(u8 taskId)
     }
 }
 
-static bool8 CanReplaceMove(void) //Allow forget HMs
+static bool8 CanReplaceMove(void) 
 {
     if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES
         || sMonSummaryScreen->newMove == MOVE_NONE
-        /*|| IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE*/)
+        || IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE)
         return TRUE;
     else
         return FALSE;
