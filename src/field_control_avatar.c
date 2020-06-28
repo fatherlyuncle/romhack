8f23b834c6a5aeb74666e876131d3c08f971ef79
fatherlyuncle <henningnick@gmail.com>
2020-06-27 22:33:12 -0400

    changed field_move_scripts.inc to fix badge prereqs on surf,wfall,dive and also remove surfmon in parrty prereq

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -446,14 +446,14 @@ static const u8 *GetInteractedMetatileScript(struct MapPosition *position, u8 me
     return NULL;
 }
 
-static const u8 *GetInteractedWaterScript(struct MapPosition *unused1, u8 metatileBehavior, u8 direction)
+static const u8 *GetInteractedWaterScript(struct MapPosition *unused1, u8 metatileBehavior, u8 direction) //removed badge and surfmon prereq
 {
-    if (FlagGet(FLAG_BADGE05_GET) == TRUE && PartyHasMonWithSurf() == TRUE && IsPlayerFacingSurfableFishableWater() == TRUE)
+    if (/*FlagGet(FLAG_BADGE05_GET) == TRUE && PartyHasMonWithSurf() == TRUE &&*/ IsPlayerFacingSurfableFishableWater() == TRUE)
         return EventScript_UseSurf;
 
-    if (MetatileBehavior_IsWaterfall(metatileBehavior) == TRUE)
+    if (MetatileBehavior_IsWaterfall(metatileBehavior) == TRUE) //removed badge prereq
     {
-        if (FlagGet(FLAG_BADGE08_GET) == TRUE && IsPlayerSurfingNorth() == TRUE)
+        if (/*FlagGet(FLAG_BADGE08_GET) == TRUE &&*/ IsPlayerSurfingNorth() == TRUE)
             return EventScript_UseWaterfall;
         else
             return EventScript_CannotUseWaterfall;
@@ -461,9 +461,9 @@ static const u8 *GetInteractedWaterScript(struct MapPosition *unused1, u8 metati
     return NULL;
 }
 
-static bool32 TrySetupDiveDownScript(void)
+static bool32 TrySetupDiveDownScript(void) // removed badge prereq
 {
-    if (FlagGet(FLAG_BADGE07_GET) && TrySetDiveWarp() == 2)
+    if (/*FlagGet(FLAG_BADGE07_GET) &&*/ TrySetDiveWarp() == 2)
     {
         ScriptContext1_SetupScript(EventScript_UseDive);
         return TRUE;
@@ -471,9 +471,9 @@ static bool32 TrySetupDiveDownScript(void)
     return FALSE;
 }
 
-static bool32 TrySetupDiveEmergeScript(void)
+static bool32 TrySetupDiveEmergeScript(void) //removed badge prereq
 {
-    if (FlagGet(FLAG_BADGE07_GET) && gMapHeader.mapType == MAP_TYPE_UNDERWATER && TrySetDiveWarp() == 1)
+    if (/*FlagGet(FLAG_BADGE07_GET) &&*/ gMapHeader.mapType == MAP_TYPE_UNDERWATER && TrySetDiveWarp() == 1)
     {
         ScriptContext1_SetupScript(EventScript_UseDiveUnderwater);
         return TRUE;
