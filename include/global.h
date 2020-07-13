5afa7527ef7f1039dd94aa4272a6915eb2804b84
fatherlyuncle <henningnick@gmail.com>
2020-07-13 02:22:59 -0400

    fixed comments to prep for saveblock expansion merge

--- include/global.h
+++ include/global.h
@@ -924,7 +924,7 @@ struct SaveBlock1
     /*0x690*/ struct ItemSlot bagPocket_TMHM[BAG_TMHM_COUNT];
     /*0x790*/ struct ItemSlot bagPocket_Berries[BAG_BERRIES_COUNT];
     /*0x848*/ struct Pokeblock pokeblocks[POKEBLOCKS_COUNT];
-    /*0x988*/ //u8 filler1[0x34]; // Previously Dex Flags, feel free to remove.
+    /*0x988*/ u8 filler1[0x34]; // Previously Dex Flags, feel free to remove.
     /*0x9BC*/ u16 berryBlenderRecords[3];
     /*0x9C2*/ u8 field_9C2[6];
     /*0x9C8*/ u16 trainerRematchStepCounter;
@@ -974,12 +974,13 @@ struct SaveBlock1
     /*0x31A8*/ u8 giftRibbons[52];
     /*0x31DC*/ struct Roamer roamer;
     /*0x31F8*/ struct EnigmaBerry enigmaBerry;
-    /*0x322C*/ //struct MEventBuffers unk_322C;// Griffin freespace rec
-    /*0x3598*/ //u8 field_3598[0x180];// Griffen freespace rec
+    /*0x322C*/ struct MEventBuffers unk_322C;
+
+    /*0x3598*/ u8 field_3598[0x180];
     /*0x3718*/ u32 trainerHillTimes[4];
     /*0x3728*/ struct RamScript ramScript;
     /*0x3B14*/ struct RecordMixingGift recordMixingGift;
-    /*0x3B24*/ //u8 seen2[DEX_FLAGS_NO];// griffin freespace rec
+    /*0x3B24*/ u8 seen2[DEX_FLAGS_NO];
     /*0x3B58*/ LilycoveLady lilycoveLady;
     /*0x3B98*/ struct TrainerNameRecord trainerNameRecords[20];
     /*0x3C88*/ u8 registeredTexts[UNION_ROOM_KB_ROW_COUNT][21];
