180f53a5fc4d4b473a906ee615620d975295930e
fatherlyuncle <henningnick@gmail.com>
2020-08-03 03:51:37 -0400

    finishing touches on mercury body

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -15268,7 +15268,8 @@ Move_MERCURY_BODY::
 	createsprite gExplosionSpriteTemplate, ANIM_ATTACKER, 3, 16, 16, 1, 1
 	delay 20
 	clearmonbg ANIM_ATTACKER
-	call UnsetPsychicBg
+	blendoff
+	restorebg
 	end
 
 Move_NANOBOTS::
